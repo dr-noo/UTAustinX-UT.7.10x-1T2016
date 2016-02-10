2dfdb006e88b91fd9482102c09faa8ec9e0a7c75
Nay Oo <oonx369@gmail.com>
2016-02-10 17:19:00 +0800

    wip Tut2

--- R-Tutorial-2.R
+++ R-Tutorial-2.R
@@ -1,1 +1,9 @@
 head(animaldata)
+table(animaldata$Sex)
+
+#bar chart for categorical variable
+plot(animaldata$Sex, main='Bar Chart of Animal Gender', xlab='Animal Gender', ylab='Frequency')
+
+#histogram plot
+hist(animaldata$Age.Intake, main='Histogram of Intake Ages', xlab='Age at Intake')
+
