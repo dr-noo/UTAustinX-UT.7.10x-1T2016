f81dceb95c5da8305d23fe51a0f892c50ab79b9a
Nay Oo <oonx369@gmail.com>
2016-02-17 10:42:09 +0800

    wip

---
+++ Pre-Lab-3.R
@@ -0,0 +1,35 @@
+library(SDSFoundations)
+bull <- BullRiders
+
+#How many of the first 10 riders in the dataset have been pro for 10 years or more?
+bull_first_10 <- bull[1:10,]
+bull_first_10_pro_10_more <-bull_first_10[bull_first_10$YearsPro>=10,]
+bull_first_10_pro_10_more
+
+#Subset for riders that participated in at least one event in 2013
+new_bull <- bull[bull$Events13  > 0 ,]
+
+# Visualize and describe the first variable of interest
+hist(new_bull$Rides13)
+fivenum(new_bull$Rides13)
+mean(new_bull$Rides13)
+sd(new_bull$Rides13)
+
+# Visualize and describe the second variable of interest
+hist(new_bull$Top10_13)
+fivenum(new_bull$Top10_13)
+mean(new_bull$Top10_13)
+sd(new_bull$Top10_13)
+
+# Create a scatterplot
+plot(new_bull$Rides13,new_bull$Top10_13)
+
+# Add line of best fit
+abline(lm(new_bull$Top10_13~new_bull$Rides13))
+
+# Calculate the correlation coefficient
+cor(new_bull$Rides13,new_bull$Top10_13)
+
+# Create a correlation matrix 
+vars <- c("Top10_13", "Rides13")
+cor(new_bull[,vars])
