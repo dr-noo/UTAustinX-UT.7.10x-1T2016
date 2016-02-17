20f94887492db6ec886b6bb14fd773e241e76974
dr-noo <noo8@gatech.edu>
2016-02-17 23:00:20 +0800

    wip

--- Lab-3.R
+++ Lab-3.R
@@ -17,3 +17,12 @@ plot(new_bull12$CupPoints12, new_bull12$Earnings12)
 nooutlier <- new_bull12[new_bull12$Earnings12 < 1000000 ,] 
 cor(nooutlier$Earnings12, nooutlier$RidePer12) # 0.8035574
 cor(nooutlier$Earnings12, nooutlier$CupPoints12) # 0.8929208
+
+minutes_studied <- c(30, 45,	180, 95, 130, 140, 30, 80, 60, 110, 0, 80)
+grade <- c(74, 68, 87, 90, 94, 84, 92, 88, 82, 93, 65, 90)
+cor(minutes_studied, grade) #  0.5967026
+plot (minutes_studied, grade)
+
+minutes_studied_outlier_removed <- c(30, 45,	180, 95, 130, 140, 80, 60, 110, 0, 80)
+grade_outlier_removed <- c(74, 68, 87, 90, 94, 84, 88, 82, 93, 65, 90)
+cor(minutes_studied_outlier_removed, grade_outlier_removed)
